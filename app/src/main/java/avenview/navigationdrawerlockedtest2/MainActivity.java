ee035eeb3d5f9847bf3d0f423aa8668d4138dc76
Ravneet Singh <30868824+JustlikeRav@users.noreply.github.com>
2017-12-06 16:35:39 -0500

    Initial commit

---
+++ app/src/main/java/avenview/navigationdrawerlockedtest2/MainActivity.java
@@ -0,0 +1,124 @@
+package avenview.navigationdrawerlockedtest2;
+
+import android.os.Bundle;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+    ListView list1, list2;
+    View lastView;
+    String[] options1 = {
+            "Start Player",
+            "Current Location",
+            "View Landmark",
+            "Add Landmarks",
+            "Default Content"
+    } ;
+    Integer[] imageId1 = {
+            R.drawable.ic_android,
+            R.drawable.ic_android,
+            R.drawable.ic_android,
+            R.drawable.ic_android,
+            R.drawable.ic_android
+
+    };
+    Integer[] selectedGradient1 = {
+            R.drawable.selected_gradient,
+            R.drawable.selected_gradient,
+            R.drawable.selected_gradient,
+            R.drawable.selected_gradient,
+            R.drawable.selected_gradient
+    };
+
+    String[] options2 = {
+            "Application Settings",
+            "Help & Feedback"
+    } ;
+    Integer[] imageId2 = {
+            R.drawable.ic_android,
+            R.drawable.ic_android
+    };
+    Integer[] selectedGradient2 = {
+            R.drawable.selected_gradient,
+            R.drawable.selected_gradient
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final panelAdapter adapter1 = new panelAdapter(MainActivity.this, options1, imageId1, selectedGradient1);
+        list1 =  findViewById(R.id.list1);
+        list1.setAdapter(adapter1);
+
+        panelAdapter adapter2 = new panelAdapter(MainActivity.this, options2, imageId2, selectedGradient2);
+        list2 =  findViewById(R.id.list2);
+        list2.setAdapter(adapter2);
+        lastView = getViewByPosition( 0, list1);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        list1.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final ViewGroup v = findViewById(R.id.lol);
+                TransitionManager.beginDelayedTransition(v);
+                if(lastView == view){
+                } else {
+                    ImageView ivSelect = view.findViewById(R.id.selectedGrad);
+                    ImageView ivLast = lastView.findViewById(R.id.selectedGrad);
+                    lastView = view;
+
+                    //Change the postion now
+                    ViewGroup.LayoutParams sizeRulesExpand = ivSelect.getLayoutParams();
+                    ViewGroup.LayoutParams sizeRulesShort = ivLast.getLayoutParams();
+
+                    //Calculate dimensions in DPs
+                    final float scale = getResources().getDisplayMetrics().density;
+
+                    sizeRulesExpand.height = (int) (50 * scale);
+                    sizeRulesExpand.width = (int) (75 * scale);
+                    ivSelect.setLayoutParams(sizeRulesExpand);
+
+                    sizeRulesExpand.height = (int) (50 * scale);
+                    sizeRulesExpand.width = (int) (7.5 * scale);
+                    ivLast.setLayoutParams(sizeRulesShort);
+                }
+            }
+        });
+
+        list2.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "You Clicked at " +options2[+ i], Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public View getViewByPosition(int pos, ListView listView) {
+        final int firstListItemPosition = listView.getFirstVisiblePosition();
+        final int lastListItemPosition = firstListItemPosition + listView.getChildCount() - 1;
+
+        if (pos < firstListItemPosition || pos > lastListItemPosition ) {
+            return listView.getAdapter().getView(pos, null, listView);
+        } else {
+            final int childIndex = pos - firstListItemPosition;
+            return listView.getChildAt(childIndex);
+        }
+    }
+}
---
+++ app/src/main/java/avenview/navigationdrawerlockedtest2/panelAdapter.java
@@ -0,0 +1,57 @@
+package avenview.navigationdrawerlockedtest2;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+class panelAdapter extends ArrayAdapter<String>
+{
+    private Activity mActivity;
+    private String[] option;
+    private Integer[] imageID, selectedGradient;
+
+    panelAdapter(Activity activity,
+                        String[] name, Integer[] imageId, Integer[] selectedGradient) {
+        super(activity, R.layout.list_row, name);
+
+        this.mActivity = activity;
+        this.option = name;
+        this.imageID = imageId;
+        this.selectedGradient = selectedGradient;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int i, View view,@NonNull ViewGroup viewGroup) {
+        View rowView = view;
+
+        if(rowView==null){
+            LayoutInflater inflater = mActivity.getLayoutInflater();
+            rowView = inflater.inflate(R.layout.list_row, null, true);
+            if(i == 0){
+                ViewGroup.LayoutParams sizeRulesExpand = ivSelect.getLayoutParams();
+
+                //Calculate dimensions in DPs
+                final float scale = getResources().getDisplayMetrics().density;
+
+                sizeRulesExpand.height = (int) (50 * scale);
+                sizeRulesExpand.width = (int) (75 * scale);
+                ivSelect.setLayoutParams(sizeRulesExpand);
+            }
+        }
+
+        TextView textView = rowView.findViewById(R.id.textEachOption);
+        ImageView imageView = rowView.findViewById(R.id.imageView);
+        ImageView selectGradient = rowView.findViewById(R.id.selectedGrad);
+
+        textView.setText(option[i]);
+        imageView.setImageResource(imageID[i]);
+        selectGradient.setImageResource(selectedGradient[i]);
+        return rowView;
+    }
+}
